7e486825eb27b54c26bb19245afbd2ad89a5b481
SuperDuke-rui <2934037629@qq.com>
2021-03-22 14:35:48 +0800

    First commit

---
+++ app/src/main/java/com/application/weather/MainActivity.java
@@ -0,0 +1,16 @@
+package com.application.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author 29340
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
